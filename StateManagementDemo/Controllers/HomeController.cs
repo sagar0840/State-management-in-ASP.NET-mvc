3b1f45ab040dd81b7fc18f82b0f57f30a3d372cf
sagar0840 <sagarkale55556@gmail.com>
2023-04-14 10:20:26 +0530

    Add project files.

---
+++ StateManagementDemo/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StateManagementDemo.Models;
+using System.Diagnostics;
+
+namespace StateManagementDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public HomeController(ILogger<HomeController> logger, IHttpContextAccessor httpContextAccessor)
+        {
+            _logger = logger;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        // working with cookie
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(IFormCollection form)
+        {
+            string email = form["email"];
+            //set cookie
+            CookieOptions options = new CookieOptions();
+            options.Expires = DateTime.Now.AddMinutes(10);
+            options.Path = "/";
+            //options.Secure = true;
+            //options.HttpOnly = true; // cookie can be read using client side script --> javascript / vbscript
+            // Response property is used to write to cookie
+            _httpContextAccessor.HttpContext.Response.Cookies.Append("email", email, options);
+
+            return RedirectToAction("ReadCookie", "ReadCookieSession");
+        }
+
+        public IActionResult WorkingWithSession()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult WorkingWithSession(IFormCollection form)
+        {
+            string email = form["email"];
+            HttpContext.Session.SetString("email", email);
+            //HttpContext.Session.SetString("roleId", "2");
+            return RedirectToAction("ReadSession", "ReadCookieSession");
+        }
+    }
+}
---
+++ StateManagementDemo/Controllers/ReadCookieSessionController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StateManagementDemo.Controllers
+{
+
+    public class ReadCookieSessionController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public ReadCookieSessionController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public IActionResult ReadCookie()
+        {
+            if (_httpContextAccessor.HttpContext != null)
+            {
+                ViewBag.Email = _httpContextAccessor.HttpContext.Request.Cookies["email"];
+            }
+            return View();
+        }
+        public IActionResult ReadSession()
+        {
+            ViewBag.Email = HttpContext.Session.GetString("email");
+            return View();
+        }
+    }
+
+}
